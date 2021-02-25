80a67e5dfdf93b013b95a0d361e019c153c906d6
electronicsgp <pedro.escom.isc@gmail.com>
2021-02-24 18:31:42 -0600

    first commit

--- pdf.php
+++ pdf.php
@@ -186,14 +186,21 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->isSMTP();       
+    $mail->SMTPDebug = 0;                                     //Send using SMTP
+    $mail->Host = gethostbyname('smtp.gmail.com');                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'zeuscaste@gmail.com';                     //SMTP username
-    $mail->Password   = 'Guapo123.';                               //SMTP password
+    $mail->Username   = 'pruebasbimcotizador@gmail.com';                     //SMTP username
+    $mail->Password   = 'Hola123.';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+        )
+        );                            //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('zeuscaste@gmail.com', 'Horizonte BIM');
